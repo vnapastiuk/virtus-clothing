30d4b8292b5bf70d6735a443ba745ef9ec55d560
sunbetkapper@gmail.com <napvital@gmail.com>
2020-04-19 00:16:59 +0200

    added redux to the project

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {auth} from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss'
@@ -18,17 +19,23 @@ const Header = ({currentUser}) => (
             <Link className='option' to='/contact'>
             CONTACT
             </Link>
-            {
-                currentUser ? 
-                <div className='option'
-                onClick={()=> auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/signin'>SIGN IN</Link>
-            }
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className='option' to='/signin'>
+          SIGN IN
+        </Link>
+      )}
 
 
         </div>
     </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state =>({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
