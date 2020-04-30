706eafa9276d95e24a711085b406c4bb7269ef12
sunbetkapper@gmail.com <napvital@gmail.com>
2020-04-30 21:08:17 +0200

    separating our cra app into client foulder, added express server to help handling payments;

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_xEOMJfpjzihbs3W0IRLDU7Hb00NF3T67bv';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successfull')
+    }).catch(error => {
+      console.log('Payment error', JSON.parse(error));
+      alert('There was an issue with your payment. Please sure you use the provided credit card');
+    })
   };
 
   return (
     <StripeCheckout
       label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      name='Virtus Clothing Ltd.'
       billingAddress
       shippingAddress
       image='https://svgshare.com/i/CUz.svg'
